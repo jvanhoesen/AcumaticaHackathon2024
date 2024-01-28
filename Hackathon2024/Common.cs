c9d202c38379cce38da17ac54616bc16c0426925
WebaholicAnonymous <administrator@webaholicanonymous.com>
2024-01-28 00:31:52 -0800

    Working button - needs clean-up

---
+++ Hackathon2024/Common.cs
@@ -0,0 +1,36 @@
+﻿using PX.Common;
+
+namespace Hackathon2024
+{
+    public static class Common
+    {
+        public static class OptimalRouteOpt
+        {
+            public const string Optimal = "Y";
+            public const string NotOptimal = "N";
+            public const string Undefined = "Z";
+
+            public class optimal : PX.Data.BQL.BqlString.Constant<optimal>
+            {
+                public optimal() : base(Optimal) {; }
+            }
+
+            public class notOptimal : PX.Data.BQL.BqlString.Constant<notOptimal>
+            {
+                public notOptimal() : base(NotOptimal) {; }
+            }
+
+            public class undefined : PX.Data.BQL.BqlString.Constant<undefined>
+            {
+                public undefined() : base(Undefined) {; }
+            }
+        }
+
+        [PXLocalizable]
+        public class HKMessages
+        {
+            public const string RouteNotOptimal = "Route is not optimal for Carbon Footprint";
+            public const string RouteOptimizationNotDefined = "Route CO2 optimization not defined";
+        }
+    }
+}
---
+++ Hackathon2024/Extensions/DAC/HKSOLineExtension.cs
@@ -0,0 +1,50 @@
+﻿using PX.Data;
+using PX.Data.BQL.Fluent;
+using PX.Objects.IN;
+using PX.Objects.SO;
+using System;
+using static Hackathon2024.Common;
+
+namespace Hackathon2024
+{
+    public class HKSOLineExtension : PXCacheExtension<SOLine>
+    {
+        public static bool IsActive() { return true; }
+
+        #region UsrCarbinFootprint
+        [PXDBDecimal]
+        [PXUIField(DisplayName = "Estimated CO2", Enabled = true)]
+        [PXParent(typeof(SOLine.FK.Order))]
+        [PXFormula(null, typeof(SumCalc<HKSOOrderExtension.usrTotalCarbonFootprint>))]
+        public virtual Decimal? UsrCarbinFootprint { get; set; }
+        public abstract class usrCarbinFootprint : PX.Data.BQL.BqlDecimal.Field<usrCarbinFootprint> { }
+        #endregion
+
+        #region UsrOptimalRoute
+        [PXDBString(1, IsFixed = true, InputMask = "")]
+        [PXUIField(DisplayName = "Optimal CO2 Route", Enabled = false)]
+        [PXStringList(
+            new string[]
+            {
+               OptimalRouteOpt.Optimal, OptimalRouteOpt.NotOptimal, OptimalRouteOpt.Undefined
+            },
+            new string[]
+            {
+                "Optimal", "Not Optimal", "Undefined"
+            })]
+        public virtual string UsrOptimalRoute { get; set; }
+        public abstract class usrOptimalRoute : PX.Data.BQL.BqlString.Field<usrOptimalRoute> { }
+        #endregion
+
+        #region UsrClosestWarehouse
+        [PXDBInt]
+        [PXUIField(DisplayName = "Closest Warehouse", Enabled = false)]
+        [PXSelector(typeof(SelectFrom<INSite>.
+                            Where<INSite.active.IsEqual<True>>.
+                            SearchFor<INSite.siteID>),
+                            SubstituteKey = typeof(INSite.siteCD))]
+        public virtual int? UsrClosestWarehouse { get; set; }
+        public abstract class usrClosestWarehouse : PX.Data.BQL.BqlInt.Field<usrClosestWarehouse> { }
+        #endregion
+    }
+}
